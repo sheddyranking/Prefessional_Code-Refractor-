37dc0e3744569291d7b3327b593d5255d109577e
sheddyranking <shedyisaac@gmail.com>
2022-01-27 20:53:12 +0100

    added the player draw attributes

--- player1.py
+++ player1.py
@@ -1,9 +1,13 @@
+import pygame
+
+
 class Player:
     def __init__(self, x, y, size, color="red"):
         self.x = x
         self.y = y
         self.size = size
         self.color = color
-        
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, (self.x, self.y, self.size, self.size))
 
         
