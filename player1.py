97ac4a37e4b8e53ddff79a628d4b4fde24b0de0b
sheddyranking <shedyisaac@gmail.com>
2022-01-27 21:09:31 +0100

    added the detection collision

--- player1.py
+++ player1.py
@@ -2,12 +2,19 @@ import pygame
 
 
 class Player:
-    def __init__(self, x, y, size, color="red"):
+    def __init__(self, x, y, size, color="red"): #playerb attributes
         self.x = x
         self.y = y
         self.size = size
         self.color = color
-    def draw(self, screen):
+
+    def draw(self, screen): #draw player attributes 
         pygame.draw.rect(screen, self.color, (self.x, self.y, self.size, self.size))
 
+    def detect_collision(self,other): #the x,y of player and enemy are incorperated into self,other.
+        if(other.x >= self.x and other.x < (self.x + self.size)) or (self.x >= other.x and self.x < (other.x + other.size)):
+            if(other.y >= self.y and other.y < (self.y + self.size)) or (self.y >= other.y and self.y < (other.y + other.size)):
+                return True
+        return False
+        
         
